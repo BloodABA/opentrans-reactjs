6d463ff3463ea1a9569790fe2aec335e2f0de60f
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-12-14 23:03:54 +0900

    ProjectHeader inner component

--- src/Components/ProjectView.js
+++ src/Components/ProjectView.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import './ProjectView.scss';
 
+
+function ProjectHeader(props) {
+    return (
+        <div className="projectHeader">
+            <div className="content">
+                <div className="icon">
+                </div>
+                <div className="summary">
+                    <div className="title">
+                        {props.title}
+                    </div>
+                    <div className="desc">
+                        {props.desc}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 class ProjectView extends Component {
     constructor(props) {
         super(props);
@@ -18,9 +38,14 @@ class ProjectView extends Component {
         return (
             <React.Fragment>
                 {this.props.index}
+                <ProjectHeader
+                    title="제목"
+                    desc="설명설명설명설명설명설명설명" />
             </React.Fragment>
         )
     }
 }
+
+
 
 export default ProjectView;
