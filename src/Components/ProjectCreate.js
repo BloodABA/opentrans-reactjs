72dc1fcd8faf65d353f85d0245936b09fdc480ee
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-12-20 23:53:15 +0900

    ProjectCreate page

--- src/Components/ProjectCreate.js
+++ src/Components/ProjectCreate.js
@@ -10,16 +10,32 @@ class ProjectCreate extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            endDate: new Date()
+            name: '',
+            id: '',
+            desc: '',
+            endDate: new Date(),
+            isOss: false
         }
         this.handleDateChange = this.handleDateChange.bind(this);
     }
-
+    handleNameInput = (e) => {
+        this.setState({name: e.target.value});
+    }
+    handleIdInput = (e) => {
+        this.setState({id: e.target.value});
+    }
+    handleDescInput = (e) => {
+        this.setState({desc: e.target.value});
+    }
+    handleOssInput = (e) => {
+        this.setState({isOss: !this.state.isOss});
+    }
     handleDateChange(date) {
-        this.setState({
-          endDate: date
-        });
-      }
+        this.setState({endDate: date});
+    }
+    projectSubmit() {
+        console.log(">>>> submit");
+    }
     
     componentDidMount() {
     }
@@ -35,34 +51,37 @@ class ProjectCreate extends Component {
                         <CardBox innerFrame title="프로젝트 생성">
                             <form className="projectForm">
                                 <div>
-                                    <label for="projectName">프로젝트명</label>
-                                    <input type="text" className="form-control" id="projectName" aria-describedby="emailHelp" placeholder="프로젝트명" />
+                                    <label htmlFor="projectName">프로젝트명</label>
+                                    <input type="text" className="form-control" id="projectName" aria-describedby="emailHelp"
+                                        placeholder="프로젝트명"  onChange={this.handleNameInput}/>
                                     <small id="projectNameHelp" className="form-text text-muted">프로젝트 이름을 입력해주세요.</small>
                                 </div>
                                 <div>
-                                    <label for="description">프로젝트 설명</label>
-                                    <input type="text" className="form-control" id="description" placeholder="프로젝트 설명" />
+                                    <label htmlFor="description">프로젝트 설명</label>
+                                    <input type="text" className="form-control" id="description"
+                                    placeholder="프로젝트 설명"  onChange={this.handleDescInput}/>
                                 </div>
                                 <div>
-                                    <label for="projectId">프로젝트 ID</label>
+                                    <label htmlFor="projectId">프로젝트 ID</label>
                                     <div className="projectId">
                                         <div className="prefix">opentrans.work/project/</div>
                                         <div className="projectIdInput">
-                                            <input type="text" className="form-control" id="projectId" placeholder="ID" />
+                                            <input type="text" className="form-control" id="projectId"
+                                                placeholder="ID"  onChange={this.handleIdInput}/>
                                         </div>
                                     </div>
                                 </div>
                                 <div>
                                     <fieldset disabled>
-                                        <label for="description">언어 번역 설정</label>
+                                        <label htmlFor="description">언어 번역 설정</label>
                                         <div className="d-flex">
                                             <input type="text" className="form-control" id="originLang" placeholder="영어" />
                                             <input type="text" className="form-control" id="transLang" placeholder="한국어" />
                                         </div>
                                     </fieldset>
                                 </div>
-                                <div className="mb-4">
-                                    <label for="description">마감일</label>
+                                <div>
+                                    <label htmlFor="description">마감일</label>
                                     <div className="endDate">
                                         <DatePicker
                                             className="form-control"
@@ -72,8 +91,15 @@ class ProjectCreate extends Component {
                                             onChange={this.handleDateChange} />
                                     </div>
                                 </div>
-                                <div className="d-flex flex-row-reverse">
-                                    <button type="submit" className="btn btn-outline-secondary">프로젝트 생성</button>
+                                <div>
+                                    <label htmlFor="isOss">오픈소스 여부</label>
+                                    <div className="isOss">
+                                        <input type="checkbox" className="mr-2" id="isOss"  onChange={this.handleOssInput}/>
+                                        <label className="form-check-label" htmlFor="isOss">이 프로젝트는 오픈소스입니다.</label>
+                                    </div>
+                                </div>
+                                <div className="d-flex flex-row-reverse mt-4">
+                                    <span className="btn btn-outline-secondary" onClick={this.projectSubmit}>프로젝트 생성</span>
                                 </div>
                             </form>
                         </CardBox>
