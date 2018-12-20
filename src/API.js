72e9f58cd3cc1e92867d3ffe137385f7be4844b7
Jangtaejin <jtjisgod@gmail.com>
2018-12-20 21:35:00 +0900

    프로젝트 리스트

--- src/API.js
+++ src/API.js
@@ -14,6 +14,10 @@ const APIUrl = {
         method : "post",
         url: baseUrl + "/account/login"
     },
+    project : {
+        method : "get",
+        url: baseUrl + "/project/"
+    }
 }
 
 
